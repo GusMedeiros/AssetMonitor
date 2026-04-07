46f78045b74306c2fefa9e335b6cf0bb48beb0a0
GusMedeiros <gustavomedeiros@id.uff.br>
2026-04-06 21:24:56 -0300

    Initial Commit

---
+++ AssetMonitor/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace AssetMonitor;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
